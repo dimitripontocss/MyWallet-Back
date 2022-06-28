e192af0d25fa0eb0f9372acd0739094492a13e07
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-06-28 16:25:50 -0300

    Registro de usuários pronto e login faltando token

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect().then(() => {
-	db = mongoClient.db("batepapo_uol");
+	db = mongoClient.db("myWallet");
 });
 
 const app = express();
@@ -19,6 +19,57 @@ app.use(cors());
 app.use(express.json());
 
 
+app.post("/signup", async (req,res)=>{
+    const {name, email, password, passwordConfirmation} = req.body;
+
+    const registerSchema = Joi.object({
+		name: Joi.string().required(),
+		email: Joi.string().required(),
+		password: Joi.string().required(),
+		passwordConfirmation: Joi.string().required()
+	});
+	const check = registerSchema.validate(req.body);
+	if(check.error){
+		return res.status(422).send("Prencha todos os campos corretamente.");
+	}
+
+	const alreadyExist = await db.collection("users").findOne({ email: email});
+	if(alreadyExist){
+		return res.status(400).send("Este email já entá cadastrado!")
+	}
+
+	if(password === passwordConfirmation){
+		await db.collection("users").insertOne({
+			name: name,
+			email: email,
+			password: password
+		});
+		return res.status(201).send("Usuário registrado com sucesso!");
+	}else{
+		return res.status(400).send("As senhas devem ser iguais!");
+	}
+})
+
+app.post("/login", async (req,res)=>{
+	const { email, password } = req.body;
+
+    const loginSchema = Joi.object({
+		email: Joi.string().required(),
+		password: Joi.string().required(),
+	});
+	const check = loginSchema.validate(req.body);
+	if(check.error){
+		return res.status(422).send("Prencha todos os campos corretamente.");
+	}
+
+	const possibleUser = await db.collection("users").findOne({ email: email});
+	if(!possibleUser){
+		return res.status(404).send("Email não cadastrado!");
+	}
+	if(possibleUser.password === password){
+		console.log()
+	}
+})
 
 
-app.listen(5000);
+app.listen(process.env.PORTA_SERVER);
