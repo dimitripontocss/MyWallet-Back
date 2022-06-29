f599b6624784b8c79efc3480ca7bbbbde24c0b60
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-06-28 23:39:46 -0300

    Parte do token implementada

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
-import { MongoClient,ObjectId } from "mongodb";
-import Joi from "joi"
+import { MongoClient, ObjectId } from "mongodb";
+import bcrypt from "bcrypt"
+import Joi from "joi";
+import { v4 as uuid } from "uuid"
 
 dotenv.config();
 
@@ -10,7 +12,7 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect().then(() => {
-	db = mongoClient.db("myWallet");
+	db = mongoClient.db(process.env.MONGO_DATABASE);
 });
 
 const app = express();
@@ -24,12 +26,12 @@ app.post("/signup", async (req,res)=>{
 
     const registerSchema = Joi.object({
 		name: Joi.string().required(),
-		email: Joi.string().required(),
+		email: Joi.string().email().required(),
 		password: Joi.string().required(),
 		passwordConfirmation: Joi.string().required()
 	});
-	const check = registerSchema.validate(req.body);
-	if(check.error){
+	const { error } = registerSchema.validate(req.body);
+	if(error){
 		return res.status(422).send("Prencha todos os campos corretamente.");
 	}
 
@@ -38,23 +40,24 @@ app.post("/signup", async (req,res)=>{
 		return res.status(400).send("Este email já entá cadastrado!")
 	}
 
-	if(password === passwordConfirmation){
-		await db.collection("users").insertOne({
-			name: name,
-			email: email,
-			password: password
-		});
-		return res.status(201).send("Usuário registrado com sucesso!");
-	}else{
+	if(password !== passwordConfirmation){
 		return res.status(400).send("As senhas devem ser iguais!");
 	}
+	const cryptedPassword = bcrypt.hashSync(password, 10);
+
+	await db.collection("users").insertOne({
+		name: name,
+		email: email,
+		password: cryptedPassword
+	});
+	return res.status(201).send("Usuário registrado com sucesso!");
 })
 
 app.post("/login", async (req,res)=>{
 	const { email, password } = req.body;
 
     const loginSchema = Joi.object({
-		email: Joi.string().required(),
+		email: Joi.string().email().required(),
 		password: Joi.string().required(),
 	});
 	const check = loginSchema.validate(req.body);
@@ -64,12 +67,64 @@ app.post("/login", async (req,res)=>{
 
 	const possibleUser = await db.collection("users").findOne({ email: email});
 	if(!possibleUser){
-		return res.status(404).send("Email não cadastrado!");
+		return res.status(401).send("Senha ou email incorretos!");
 	}
-	if(possibleUser.password === password){
-		console.log()
+	const passwordValidate = bcrypt.compareSync(password, possibleUser.password);
+	if(passwordValidate){
+		const token = uuid();
+
+		await db.collection("sessions").insertOne({
+			token,
+			userId: possibleUser._id
+		})
+		return res.status(201).send(
+			{
+				token: token,
+				name:possibleUser.name
+			});
+	}else{
+		return res.status(401).send("Senha ou email incorretos!");
 	}
 })
 
+app.get("/register", async (req,res)=>{
+	const { authorization } = req.headers;
+	const token = authorization?.replace("Bearer ", "");
 
-app.listen(process.env.PORTA_SERVER);
+	const session = await db.collection("sessions").findOne({ token });
+	if(!session){
+		return res.sendStatus(401);
+	}
+    
+	const userRegisters = await db.collection("registers").find({userId: new ObjectId(session.userId)}).toArray();
+	res.status(200).send(userRegisters);
+})
+
+app.post("/register", async (req,res)=>{
+	const { authorization } = req.headers;
+	const token = authorization?.replace("Bearer ", "");
+	const { amount, description, type} = req.body;
+
+	const registerSchema = Joi.object({
+		amount: Joi.number().required(),
+		description: Joi.string().required(),
+		type: Joi.string().required().valid("income", "outcome")
+	})
+
+	const { error } = registerSchema.validate(req.body);
+	if(error){
+		return res.sendStatus(422);
+	}
+	
+	const session = await db.collection("sessions").findOne({ token });
+	if(!session){
+		return res.sendStatus(401);
+	}
+    
+	await db.collection("registers").insertOne({ ...req.body, userId: session.userId});
+	res.status(200).send("Transação concluída!");
+})
+
+
+const PORT = process.env.PORTA_SERVER || 5001;
+app.listen(PORT);
